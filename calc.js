cb612c5d0daf2f3d48ad7a6cad05b073271608d5
schuylermk <schuyler.m.k@gmail.com>
2018-04-09 16:00:55 -0700

    Handling key presses and calculation

--- calc.js
+++ calc.js
@@ -1,62 +1,106 @@
-"use strict";
+"use strict"
 
-// Get value of button that was pressed
+const calculate = (n1, operator, n2) => {
+  let result = ''
+  if (operator === 'add') {
+    result = parseFloat(n1) + parseFloat(n2)
+  } else if (operator === 'subtract') {
+    result = parseFloat(n1) - parseFloat(n2)
+  } else if (operator === 'multiply') {
+    result = parseFloat(n1) * parseFloat(n2)
+  } else if (operator === 'divide') {
+    result = parseFloat(n1) / parseFloat(n2)
+  }
 
-const calculator = document.querySelector('.calculator');
-const display = calculator.querySelector('input');
-const keys = calculator.querySelector('.keypad');
-
-keys.addEventListener('click', e => {
-  const key = e.target;
-  const action = key.dataset.action;
-
-  // Get type of key pressed
-  
-  if (!action) {
-    console.log(e.target.value)
-  }
-  if (action === 'add') {
-    console.log('...plus...')
-  }
-  if (action === 'subtract') {
-    console.log('...minus...')
-  }
-  if (action === 'multiply') {
-    console.log('...times...')
-  }
-  if (action === 'divide') {
-    console.log('...divided by...')
-  }
-  if (action === 'percent') {
-    console.log('...percent of...')
-  }
-  if (action === 'calculate') {
-    console.log('...equals...')  
-  }
-  if (action === 'clear') {
-    console.log('Memory cleared!')  
-  }
-});
-
-// Append first number (operand) to the display
-function displayOperand(e) {
-  const key = document.querySelector(`.key[value="${e.target.value}"]`)
-  const display = document.querySelector(`.display[value="${e.target.value}"]`)
-  display.innerHTML()
+  return result
 }
 
+const calculator = document.querySelector('.calculator')
+const display = calculator.querySelector('.display')
+const keys = calculator.querySelector('.keypad')
 
 
+keys.addEventListener('click', e => {
+  if (e.target.matches('button')) {
+    const key = e.target
+    const action = key.dataset.action
+    const keyContent = key.textContent
+    const displayedNum = display.textContent
+    const previousKeyType = calculator.dataset.previousKeyType
+    
+    Array.from(key.parentNode.children)
+      .forEach(k => k.classList.remove('is-pressed'))
 
-// Set operator (and possibly append previous operand(s) to smaller, upper display)
+// Number key pressed...
+  if (!action) { 
+    console.log(`Number key ${key.innerHTML} was pressed!`)
+    
+    calculator.dataset.previousKey = 'number'
+    if (displayedNum === '0' || previousKeyType ==='operator') {
+      display.textContent = keyContent
+      } else {
+        display.textContent = displayedNum + keyContent 
+      }
+  }
 
+// Decimal key pressed    
+  if (action === 'decimal') {
+    console.log(`.decimal key.`)
 
-// Append new number (operand) to the display
+    if (!displayedNum.includes('.')) {
+      display.textContent = displayedNum + '.'
+    } else if (previousKeyType === 'operator') {
+      display.textContent = '0.'
+    }
 
+    calculator.dataset.previousKey = 'decimal'
+  }
 
-                //   _by pressing new operator (with respect to OoO)
-// CALCULATE result {
-              //     ¯by pressing equals sign
+// Operator key pressed...
+  if (
+    action === 'add' ||
+    action === 'subtract' ||
+    action === 'multiply' ||
+    action === 'divide' ||
+    action === 'sqroot' ||
+    action === 'percent'
+  ){  
+    const firstNumber = calculator.dataset.firstNumber
+    const operator = calculator.dataset.operator
+    const secondNumber = displayedNum
+    console.log(`${key.getAttribute('data-action')} key was pressed`)
+    
+    if (firstNumber && operator) {
+      display.textContent = calculate(firstNumber, operator, secondNumber)
+    }
 
-window.addEventListener('keydown', displayOperand);              
-window.addEventListener('click', displayOperand);              
+    key.classList.add('is-pressed')
+    calculator.dataset.previousKeyType = 'operator'
+    calculator.dataset.firstNumber = displayedNum
+    calculator.dataset.operator = action
+
+    
+
+  }
+
+// Clear key pressed    
+    if (action === 'clear') {
+      console.log(`All clear`)
+      calculator.dataset.previousKeyType = 'clear'
+
+      display.textContent = `0`
+    }
+
+// Equals key pressed    
+    if (action === 'calculate') {
+      console.log(`Equals`)
+
+      const firstNumber = calculator.dataset.firstNumber
+      const operator = calculator.dataset.operator
+      const secondNumber = displayedNum
+
+      display.textContent = calculate(firstNumber, operator, secondNumber)
+    }
+  }
+})
+
